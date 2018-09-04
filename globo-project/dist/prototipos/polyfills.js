8ec9ba0ae9644faf415aa2e5f795c0b60333bd8a
= <pirsin_3000@hotmail.com>
2018-09-04 13:33:08 -0300

    nada

--- globo-project/dist/prototipos/polyfills.js
+++ globo-project/dist/prototipos/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Jesus Llaya\Desktop\Angular\GP-Prototipos-VersionFinal\globo-project\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Jesus Llaya\Documents\GitHub\Akira\globo-project\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
